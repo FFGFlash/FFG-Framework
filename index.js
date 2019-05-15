018a6f8e5dc3d5aa7a358971a224b0646dc3709f
FFGFlash <tayld.ainjastudios@gmail.com>
2019-05-15 01:38:40 -0500

    Fixed index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
 	Discord: require("./discord.js"),
 	Console: require("./console.js"),
 	Bitdata: require("./bitdata.js"),
-	EventHandler: require("./event.js");
+	EventHandler: require("./event.js")
 };
